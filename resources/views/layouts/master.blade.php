1526f28760e542217489b9031597ef58d041c16d
Noman Abdullah <71120198+nomanabdullah-dev@users.noreply.github.com>
2021-08-26 02:37:25 +0600

    admin template mastering done

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+@include('layouts.partials._head')
+
+<body class="hold-transition sidebar-mini">
+
+    <div class="wrapper">
+
+        @include('layouts.partials._navbar')
+        @include('layouts.partials._sidebar')
+
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            
+        </div>
+        <!-- /.content-wrapper -->
+
+        @include('layouts.partials._footer')
+    </div>
+    <!-- ./wrapper -->
+
+    @include('layouts.partials._footer-script')
+
+</body>
+</html>
